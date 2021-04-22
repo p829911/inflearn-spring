b73084befa2da318a4de5b313d5e6a37c7ecfdb5
p829911 <p829911@gmail.com>
2021-04-22 20:55:01 +0900

    Code updates related to singleton.

---
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,41 @@
+package hello.core.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingleton() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        // ThreadA: A 사용자 10000원 주문
+        int userAPrice = statefulService1.order("userA", 10000);
+
+        // ThreadB: B 사용자 20000원 주문
+        int userBPrice = statefulService2.order("userB", 20000);
+
+        // ThreadA: 사용자A 주문 금액 조회
+        System.out.println("price = " + userAPrice);
+
+        // ThreadB: 사용자B 주문 금액 조회
+//        int price2 = statefulService2.getPrice();
+
+        Assertions.assertThat(userAPrice).isEqualTo(10000);
+    }
+
+    static class TestConfig {
+
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+}
