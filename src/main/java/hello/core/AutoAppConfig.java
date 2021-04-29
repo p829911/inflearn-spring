1ae98664a7205ccb2ff3f9ae50651704e743025f
p829911 <p829911@gmail.com>
2021-04-29 13:14:47 +0900

    @ComponentScan options

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core.member", // 어디서 부터 찾는지 지정
+//        basePackageClasses = AutoAppConfig.class, // 클래스가 존재하는 패키지 부터 찾는다
+        // 지정하지 않으면 @ComponentScan 붙어있는 클래스 패키지 부터 하위 패키지 탐색한다
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
